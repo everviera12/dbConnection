513a74b656857c45b90a5f77f4876025db67f741
EverViera12 <evertigre10@gmail.com>
2023-07-11 14:37:56 -0600

    Agregar archivos de proyecto.

---
+++ dbConnection/Form1.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dbConnection
+{
+    public partial class Form1 : Form
+    {
+        private connection mConexion;
+        public Form1()
+        {
+            InitializeComponent();
+            mConexion = new connection();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            string result = ""; 
+            MySqlDataReader MySqlDataReader = null;
+            string consulta = "CALL verUsuarios";
+            if (mConexion.GetSqlConnection() != null)
+            {
+                MySqlCommand mySqlCommand = new MySqlCommand(consulta);
+                mySqlCommand.Connection = mConexion.GetSqlConnection();
+                MySqlDataReader = mySqlCommand.ExecuteReader();
+                while (MySqlDataReader.Read())
+                {
+                    result = MySqlDataReader.GetString("name");
+                }
+                MessageBox.Show(result);
+            }
+            else
+            {
+                MessageBox.Show("Error de conexion: ");
+            }
+        }
+    }
+}
---
+++ dbConnection/connection.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbConnection
+{
+    class connection
+    {
+        // datos de la base de datos
+        private MySqlConnection conexion;
+        private String server = "127.0.0.1";
+        private String database = "db_test";
+        private String user = "root";
+        private String password = "root123";
+        private String cadenaConnection;
+
+        //creamos un constructor para tener la cadena de la base de datos
+        public connection()
+        {
+            cadenaConnection = "Database = " + database + ";" +
+                "DataSource = " + server + ";" +
+                "User ID = " + user + ";" +
+                "Password = " + password;
+        }
+
+        // verificamos si la conexion está correcta
+        public MySqlConnection GetSqlConnection()
+        {
+            if (conexion == null)
+            {
+                conexion = new MySqlConnection(cadenaConnection);
+                conexion.Open();
+            }
+            return conexion;
+        }
+    }
+}
